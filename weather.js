b14e186af99313b0f216a47aefeebe19259e548b
priyanka-chokkalla <79966169+priyanka-chokkalla@users.noreply.github.com>
2021-03-17 15:23:57 +0530

    Create weather.js

---
+++ weather.js
@@ -0,0 +1,61 @@
+console.log("get weather function invoked");
+async function getWeather(){
+    console.log("get weather function invoked2");
+    var city=document.getElementById("city").value;
+    var url="https://api.openweathermap.org/data/2.5/forecast?q=";
+    var key="a30c8c95a5d37aaf5ba0d2a3802d4525";
+    url=url.concat(city).concat('&appid=').concat(key).concat('&units=metric');
+    console.log(url)
+    let result=await fetch(url);
+    let ob=await result.json();
+    var a=document.getElementById("display");
+    a.innerHTML='<p id="country">'+"COUNTRY NAME: "+'</p><br>'+'<p id="max_temp">'+"MAXIMUM TEMPERATURE IS : "+ob.list[0].main.temp_max+'</p><br>'+
+    '<p id="min_temp">'+"MINIMUM TEMPERATURE IS : "+ob.list[0].main.temp_min+'</p><br>'+
+    '<p id="humidity">'+"HUMIDITY : "+ob.list[0].main.humidity+'</p><br>';
+    label=[];
+    data=[];
+    var time=ob.list[0].dt_txt.split(" ")[1];
+  
+    for(i=0;i<40;i+=8){
+        var date=ob.list[i].dt_txt.split(" ");
+        label.push(date[0]);
+        data.push(ob.list[i].main.temp);
+    }
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: label,
+            data: data,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(54, 162, 235, 0.5)',
+                'rgba(255, 206, 86, 0.5)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 0.5)',
+                'rgba(255, 159, 64, 0.5)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(54, 162, 235, 0.5)',
+                'rgba(255, 206, 86, 0.5)',
+                'rgba(75, 192, 192, 0.5)',
+                'rgba(153, 102, 255, 0.5)',
+                'rgba(255, 159, 64, 0.5)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+}
